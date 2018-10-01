63ba5c660ec775d00a9e9571d915bcb5f0adef85
Alexei Dariev <alexeydariev@gmail.com>
2018-10-01 12:35:05 +0300

    first commit

---
+++ SignalR_app/Models/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace SignalR_app.Models
+{
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationContext() : base("Database1") { }
+
+        public static ApplicationContext Create()
+        {
+            return new ApplicationContext();
+        }
+    }
+}
